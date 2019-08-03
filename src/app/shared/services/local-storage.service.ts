78257a2185241ca9d9f4180c73a6b4ff259d2968
Gustavo I. Chalco <gchalcoa@gmail.com>
2019-08-03 14:03:56 -0500

    service for store in localstorage

---
+++ src/app/shared/services/local-storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { RateResponseExchange } from '../model/RateResponseExchange';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  private localStorageKey = '_ExchangeStorage';
+  private localStorageTTL = '_ExchamgeStorageTTL';
+  constructor() { }
+
+  saveStorage(state: RateResponseExchange) {
+    localStorage.setItem(this.localStorageTTL, state.timestamp.toString());
+    localStorage.setItem(this.localStorageKey, JSON.stringify( state.rates ) );
+  }
+
+  removeStorage() {
+    localStorage.removeItem(this.localStorageKey);
+    localStorage.removeItem(this.localStorageTTL);
+  }
+
+}
