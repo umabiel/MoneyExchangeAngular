3213adb43f4c3469a39f236ea01ca0c3e5957beb
Gustavo I. Chalco <gchalcoa@gmail.com>
2019-08-03 18:08:09 -0500

    get rate for exchange

--- src/app/shared/services/exchange.service.ts
+++ src/app/shared/services/exchange.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { RateResponseExchange } from '../model/RateResponseExchange';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExchangeService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService) {}
 
   getExchange() {
     return this.http.get<RateResponseExchange>(
       'http://data.fixer.io/api/latest?access_key=33b23d6e01efe285daf21f65e1124757'
     );
+  }
+
+  getRateForExchange(rate: string): number {
+    const rates = this.localStorageService.getRates();
+    return rates[rate];
   }
 }
