6633f396f8acb92211fbe1a95d8ec9ce8ce5d44e
Gustavo I. Chalco <gchalcoa@gmail.com>
2019-08-03 18:10:17 -0500

    changes for operation on components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExchangeService } from './shared/services/exchange.service';
+import { LocalStorageService } from './shared/services/local-storage.service';
+import { RateResponseExchange } from './shared/model/RateResponseExchange';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MoneyExchangeAngular';
+export class AppComponent implements OnInit {
+
+  private money: number;
+  public rate: RateResponseExchange = { base: '', rates: [] };
+  public resultExchange: number;
+
+  constructor(
+    private exchangeService: ExchangeService,
+    private locaStorageService: LocalStorageService
+  ) {}
+
+  ngOnInit() {
+    this.exchangeService.getExchange().subscribe((res: RateResponseExchange) => {
+      this.rate = res;
+      this.locaStorageService.saveStorage(res);
+      console.log('RATE ', this.exchangeService.getRateForExchange('USD'));
+    });
+  }
+
+  getmoney(money: number) {
+    this.money = money;
+  }
+
+  calculate() {
+    const rate = this.exchangeService.getRateForExchange('USD');
+    console.log(rate * this.money);
+    this.resultExchange = rate * this.money;
+  }
+
 }
