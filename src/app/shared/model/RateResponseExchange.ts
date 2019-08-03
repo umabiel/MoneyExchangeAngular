da942ef59fff051efc830bc5bfb1f0a3511213e0
Gustavo I. Chalco <gchalcoa@gmail.com>
2019-08-03 18:08:26 -0500

    optionals

--- src/app/shared/model/RateResponseExchange.ts
+++ src/app/shared/model/RateResponseExchange.ts
@@ -1,7 +1,7 @@
 import { Rate } from './Rate';
 export interface RateResponseExchange {
-  success: boolean;
-  timestamp: number;
+  success?: boolean;
+  timestamp?: number;
   base?: string;
   date?: Date;
   rates?: Rate[];
